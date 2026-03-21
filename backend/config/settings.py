58d986f5188ff7a4dafd32e6dc63656e28899b33
muhammed-anees-pp <muhammedanees.work@gmail.com>
2026-03-21 18:56:43 +0530

    build(db): use SQLite3 instead of AWS RDS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -100,17 +100,22 @@ ASGI_APPLICATION = "config.asgi.application"
 # --------------------
 # DATABASE
 # --------------------
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # -------------------------------------------------
 # PASSWORD VALIDATION
 # -------------------------------------------------
